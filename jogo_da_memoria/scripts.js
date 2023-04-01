92037e742e1d9a8e28ba7a2b1438df44fcab92d0
João Pedro <joaopedrobragagomes3@gmail.com>
2023-04-01 16:52:45 -0300

    segundo commit

---
+++ jogo_da_memoria/scripts.js
@@ -0,0 +1,99 @@
+// Iniciar Game
+
+function startGame(){
+
+    // Selecionando todas as cartas
+    const cards = document.querySelectorAll('.memory-card');
+  
+    let hasFlippedCard = false;
+    let lockBoard = false;
+    let firstCard, secondCard;
+    var countForWin = 0;
+  
+    // Função para virar carta
+    function flipCard() {
+  
+      if (lockBoard) return;
+      if (this === firstCard) return;
+  
+      this.classList.add('flip');
+  
+      if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+  
+        return;
+      }
+  
+      secondCard = this;
+      checkForMatch();
+    }
+  
+    //  Verificar se as cartas são iguais
+    function checkForMatch() {
+      let isMatch = firstCard.dataset.aeronave === secondCard.dataset.aeronave;
+  
+      isMatch ? disableCards() : unflipCards();
+  
+    }
+  
+    // Se sim, deixar cartas para cima e contar pares formados
+    function disableCards() {
+      countForWin++;
+  
+      if(countForWin === 6){
+  
+        setTimeout(mens,1200);
+  
+          function mens(){
+          header = document.querySelector('header').style.display = 'none';
+          section = document.querySelector('section').style.display = 'none';
+          body = document.querySelector('body');
+  
+          h1 = '<h1 id="win">PARABÉNS!! VOCÊ GANHOU!!</h1>'
+  
+          body.innerHTML = h1;
+          
+          setTimeout(()=>{
+              self.location = 'index.html';
+          }, 2500)
+  
+          return;
+        }
+      }
+  
+      firstCard.removeEventListener('click', flipCard);
+      secondCard.removeEventListener('click', flipCard);
+  
+      resetBoard();
+    }
+  
+    // Retornar carta para baixo
+    function unflipCards() {
+      lockBoard = true;
+  
+      setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+  
+        resetBoard();
+      }, 1500);
+    }
+  
+    // Resetar Painel
+    function resetBoard() {
+      [hasFlippedCard, lockBoard] = [false, false];
+      [firstCard, secondCard] = [null, null];
+    }
+  
+    (function shuffle() {
+      cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+      });
+    })();
+  
+    // Aplicar função flipCard para lista de cartas obtidas
+    cards.forEach(card => card.addEventListener('click', flipCard));
+  
+  }
---
+++ jogo_da_memoria/timer.js
@@ -0,0 +1,83 @@
+// Coletando meus elementos
+var header = document.querySelector('header');
+var section = document.querySelector('section');
+var body = document.querySelector('body');
+var btStart = document.querySelector('#start');
+
+// Função pra configurar e decrementar o tempo
+const contarTempo = (duracao, display) => {
+    var timer = duracao, minutos, segundos;
+
+    function contador(){
+        minutos = parseInt(timer / 60, 10);
+        segundos = parseInt(timer % 60, 10);
+
+        minutos = minutos < 10 ? "0" + minutos : minutos;
+        segundos = segundos < 10 ? "0" + segundos : segundos;
+
+        display.textContent = minutos + ":" + segundos;
+        // return console.log(minutos + ":" + segundos);
+
+        --timer;
+
+        // Se timer estiver nos 10 segundo finais
+        if(timer < 10 && timer >= 0){
+            display.style.color = 'red';
+            display.style.fontWeight = '600';
+        }
+        // Se timer acabar
+        else if( timer < 0){
+
+            header.style.display = 'none';
+            section.style.display = 'none';
+
+            h1 = '<h1 id="lost">O tempo acabou! você perdeu!!</h1>'
+
+            body.innerHTML = h1;
+            
+            setTimeout(()=>{
+                self.location = 'index.html';
+            }, 2500)
+            
+        } 
+    }
+
+    setInterval(contador,1000);
+}
+
+
+ // Função de mensagem incial para começar jogo
+ const changeTextButton = () => {
+
+    btStart.textContent = 'Encontre as cartas';
+    btStart.classList.add('encontre');
+    return;
+}
+
+// Função para retornar a tela inicial depois da mensagem acima
+function retonar() {
+    h1 = document.getElementById('win');
+    h1.style.display = 'none';
+
+    body.insertBefore(header, body.children[0]);
+    body.insertBefore(section, body.children[1]);
+
+    header.style.display = 'flex';
+    
+    return;
+}
+
+
+
+// Se acionado o botão start
+btStart.addEventListener('click', () =>{
+
+    var duracao = 70;
+
+    var display = document.getElementById('timer');
+
+    changeTextButton();
+
+    contarTempo(duracao, display);
+
+})
